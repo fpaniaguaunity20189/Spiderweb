9f7d94f389125f5daf60d3bdb8130c0dd8f9434d
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-11-21 11:16:07 +0100

    Initial commit

---
+++ Assets/RopeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour {
+    LineRenderer lr;
+    public Transform spiderTransform;
+	// Use this for initialization
+	void Start () {
+        lr = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lr.SetPosition(1, spiderTransform.position);
+	}
+}
